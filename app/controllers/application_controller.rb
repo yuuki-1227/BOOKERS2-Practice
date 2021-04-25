bf866c72dbafface10ed5862e6c7967f7a01452e
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-25 08:47:31 +0000

    []

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.petmit(:sign_up, key:[:name])
+  end
+  
 end
