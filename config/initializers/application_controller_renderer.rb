fb751e99417cdc3a0bb758bf1b4e5e59fcebc109
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-26 07:32:26 +0000

    [Add]編集機能、削除機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '79c08a52a1fd38f2a5ce2bdf27fad311a20008ca3cf143cb63f63184ded61c607617909328ee82f1863f5aedcc679deb186c8aaaa7ce6014fd5a85d4d2cbe099'
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
 
-  resources :users ,only:[:index, :show]
+  resources :users ,only:[:index, :show, :edit, :update]
   resources :books ,only:[:index, :create, :show, :update, :edit, :destroy]
 
 
